e94b3d0d4a9d19e913cd4a6ed45b20be03045a5c
HBpencil <71178397+warlockhb@users.noreply.github.com>
2025-02-17 11:51:37 +0900

    💻Init: 프로젝트 첫 생성 및 아트 리소스 추가

---
+++ Source/Project_GGF/Project_GGF.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Project_GGF : ModuleRules
+{
+	public Project_GGF(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
