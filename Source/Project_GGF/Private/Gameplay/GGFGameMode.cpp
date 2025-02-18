c3f85127e22c52ac5aec4c5e08daa9c3522a3658
yujin1025 <kyjc011006@gmail.com>
2025-02-18 13:40:08 +0900

    🎉Add: GameMode, GameState 생성

---
+++ Source/Project_GGF/Private/Gameplay/GGFGameMode.cpp
@@ -0,0 +1,30 @@
+﻿#include "Gameplay/GGFGameMode.h"
+#include "Gameplay/GGFGameState.h"
+
+AGGFGameMode::AGGFGameMode()
+{
+	GameStateClass = AGGFGameState::StaticClass();
+}
+
+void AGGFGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    GetWorldTimerManager().SetTimer(GameTimeHandle, this, &AGGFGameMode::UpdateGameTime, 1.0f, true);
+}
+
+void AGGFGameMode::UpdateGameTime()
+{
+    CurrentTime += 1.0f;
+
+    if (CurrentTime >= TotalGameTime)
+    {
+        UE_LOG(LogTemp, Error, TEXT("베드 엔딩"));
+        // 게임 종료 구현하기
+    }
+}
+
+float AGGFGameMode::GetGameTimeRatio() const
+{
+    return FMath::Clamp(CurrentTime / TotalGameTime, 0.0f, 1.0f);
+}
---
+++ Source/Project_GGF/Public/Gameplay/GGFGameMode.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GGFGameMode.generated.h"
+
+UCLASS()
+class PROJECT_GGF_API AGGFGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AGGFGameMode();
+
+    float GetGameTimeRatio() const;
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    UPROPERTY(EditAnywhere)
+    float TotalGameTime = 900.0f; 
+    float CurrentTime = 0.0f;
+
+    FTimerHandle GameTimeHandle;
+    void UpdateGameTime();
+};
