de6b2fb57f4949ad870a284feb846d20cc9cf978
yujin1025 <kyjc011006@gmail.com>
2025-02-19 12:19:26 +0900

    🎉Add: 동물 애니메이션 구현
    ABP, BS 생성 + BP 수정

--- Source/Project_GGF/Private/AI/Creatures/Animal.cpp
+++ Source/Project_GGF/Private/AI/Creatures/Animal.cpp
@@ -1,21 +1,68 @@
-#include "AI/Creatures/Animal.h"
+﻿#include "AI/Creatures/Animal.h"
+#include "AIControllerCustom.h"
+#include "Gameplay/GGFGameMode.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 AAnimal::AAnimal()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+    AIControllerClass = AAIControllerCustom::StaticClass();
+    AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 }
 
 void AAnimal::BeginPlay()
 {
 	Super::BeginPlay();
 	
+    GameMode = Cast<AGGFGameMode>(GetWorld()->GetAuthGameMode());
+
+    AAIControllerCustom* AIController = Cast<AAIControllerCustom>(GetController());
+    if (AIController)
+    {
+        BlackboardComponent = AIController->GetBlackboardComponent();
+    }
 }
 
 void AAnimal::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+    if (GameMode)
+    {
+        UpdateTimeState();
+    }
 }
 
 
+void AAnimal::UpdateTimeState()
+{
+    float TimeRatio = GameMode->GetGameTimeRatio();
+
+    BlackboardComponent->SetValueAsBool(TEXT("bIsNight"), (TimeRatio >= 0.7f));
+    BlackboardComponent->SetValueAsBool(TEXT("bIsEvening"), (TimeRatio > 0.4f && TimeRatio < 0.7f));
+    BlackboardComponent->SetValueAsBool(TEXT("bIsDay"), (TimeRatio <= 0.4f));
+}
+
+void AAnimal::UpdateAttackState(bool bIsHit)
+{
+}
+
+
+void AAnimal::OnDeath()
+{
+    /* 캐릭터 받아서 캐릭터에 additemtoinventory 함수 호출해서 그 안에서 quest의 UpdateQuestProgress 호출하도록 하기
+    APlayerCharacter* Player = Cast<APlayerCharacter>(GetWorld()->GetFirstPlayerController()->GetPawn());
+    if (Player)
+    {
+        for (const FAnimalLoot& Loot : LootTable)
+        {
+            FString ItemName = Loot.ItemData.ItemName; 
+            int32 Quantity = Loot.Quantity;
+
+            Player->AddItemToInventory(ItemName, Quantity);
+        }
+    }*/
+
+    Destroy();
+}
--- Source/Project_GGF/Public/AI/Creatures/Animal.h
+++ Source/Project_GGF/Public/AI/Creatures/Animal.h
@@ -2,7 +2,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Gameplay/Quest/QuestItemData.h"
 #include "Animal.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAnimalLoot
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Loot")
+	FQuestItemData ItemData; 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Loot")
+	int32 Quantity; 
+};
 
 UCLASS()
 class PROJECT_GGF_API AAnimal : public ACharacter
@@ -12,10 +25,27 @@ class PROJECT_GGF_API AAnimal : public ACharacter
 public:
 	AAnimal();
 
+	UFUNCTION(BlueprintCallable)
+	TArray<FAnimalLoot> GetLoot() const { return LootTable; }
+
 protected:
 	virtual void BeginPlay() override;
-
-public:	
 	virtual void Tick(float DeltaTime) override;
+	virtual void OnDeath();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Loot")
+	TArray<FAnimalLoot> LootTable;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	UAnimMontage* DrinkMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	UAnimMontage* SleepMontage;
+
+private:
+	void UpdateTimeState();
+	void UpdateAttackState(bool bIsHit);
+
+	class AGGFGameMode* GameMode;
+	class UBlackboardComponent* BlackboardComponent;
 };
