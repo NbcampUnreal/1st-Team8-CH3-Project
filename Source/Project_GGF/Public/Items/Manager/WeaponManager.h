9b5a980bf07ce273454194e3e4db5dec2d6f9244
HBpencil <71178397+warlockhb@users.noreply.github.com>
2025-02-21 14:58:47 +0900

    🩹Fix: WeaponManager.h 누락 코드 변경

--- Source/Project_GGF/Public/Items/Manager/WeaponManager.h
+++ Source/Project_GGF/Public/Items/Manager/WeaponManager.h
@@ -2,26 +2,39 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "Items/Weapon/Weapon.h"
 #include "WeaponManager.generated.h"
 
-class AWeapon;
+//class AWeapon;
+class ACharacter;
 
 UCLASS(Blueprintable, BlueprintType)
 class PROJECT_GGF_API UWeaponManager : public UObject
 {
 	GENERATED_BODY()
-
+	
 private:
+	int32 CurrentIdx;
+	int32 MaxIdx;
+
+	ACharacter* Owner;
+
+public:
+	UPROPERTY(EditAnywhere)
+	TArray<TSubclassOf<AWeapon>> WeaponClasses;
+	
 	UPROPERTY(EditAnywhere)
 	TArray<AWeapon*> Weapons;
-	int32 CurrentIdx;
 
 public:
 	bool Attack();
 	bool Reload();
-	AWeapon* ChangeWeapon(int32 _Idx);
+	bool ChangeWeapon(int32 _Idx);
 
 	void AddWeapon(AActor* _Actor);
+
+	void CreateWeapons(ACharacter* _Owner);
+	
 public:
 	UWeaponManager();
 	~UWeaponManager();
