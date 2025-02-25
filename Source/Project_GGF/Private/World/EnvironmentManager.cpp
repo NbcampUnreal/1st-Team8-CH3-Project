32eed11a5d21e8fd9c1f7e5cc211ab11648e4827
HBpencil <71178397+warlockhb@users.noreply.github.com>
2025-02-26 03:14:45 +0900

    🎉Add: EnvironmentManager 및 블루프린트 추가

---
+++ Source/Project_GGF/Private/World/EnvironmentManager.cpp
@@ -0,0 +1,442 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "World/EnvironmentManager.h"
+#include "MaterialHLSLTree.h"
+
+
+// Sets default values
+AEnvironmentManager::AEnvironmentManager()
+{
+	//Initialize
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// RootComponent
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	// Sun
+	Sun = CreateDefaultSubobject<UDirectionalLightComponent>(TEXT("Sun"));
+	Sun->SetupAttachment(RootComponent);
+
+	// Moon
+	Moon = CreateDefaultSubobject<UDirectionalLightComponent>(TEXT("Moon"));
+	Moon->SetupAttachment(Sun);
+
+	// Cloud
+	Cloud = CreateDefaultSubobject<UVolumetricCloudComponent>(TEXT("Cloud"));
+	Cloud->SetupAttachment(RootComponent);
+
+	//SkyLight
+	SkyLight = CreateDefaultSubobject<USkyLightComponent>(TEXT("SkyLight"));
+	SkyLight->SetupAttachment(RootComponent);
+
+	// Fog
+	Fog = CreateDefaultSubobject<UExponentialHeightFogComponent>(TEXT("Fog"));
+	Fog->SetupAttachment(RootComponent);
+
+	// 포스트 프로세스
+	PostProcess = CreateDefaultSubobject<UPostProcessComponent>(TEXT("PostProcess"));
+	PostProcess->SetupAttachment(RootComponent);
+
+	// 포스트 프로세스 설정 활성화
+	PostProcess->Settings.bOverride_WhiteTemp = true;
+	
+	PostProcess->Settings.bOverride_ColorSaturation = true;
+	PostProcess->Settings.bOverride_ColorContrast = true;
+	PostProcess->Settings.bOverride_ColorGamma = true;
+	PostProcess->Settings.bOverride_ColorGain = true;
+	PostProcess->Settings.bOverride_ColorOffset = true;
+
+	PostProcess->Settings.bOverride_AutoExposureMinBrightness = true;
+	PostProcess->Settings.bOverride_AutoExposureMaxBrightness = true;
+
+	// Sky Attmosphere
+	SkyAtmosphere = CreateDefaultSubobject<USkyAtmosphereComponent>("SkyAtmosphere");
+	SkyAtmosphere->SetupAttachment(RootComponent);
+
+	// Sky Mesh
+	SkyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SkyMesh"));
+
+	
+	// Initialize
+	//Time
+	//시간 검사
+	SetCurrentTime(CurrentTime);
+	
+	// Sun
+	Sun->Intensity = 4.0f;
+	Sun->AtmosphereSunLightIndex = 0;
+	Moon->ForwardShadingPriority = 0;
+	
+	// Moon
+	// 해 라이트와 반대급부 위치
+	Moon->SetWorldRotation(FRotator(180, 0, 0));
+	Moon->SetVisibility(true);
+	
+	Moon->Intensity = 0.25f;
+	Moon->LightSourceAngle = 0.0f;
+	Moon->AtmosphereSunLightIndex = 1;
+	Moon->ForwardShadingPriority = 1;
+	
+	Moon->bUseTemperature = true;
+	Moon->Temperature = 12000.0f;
+	
+	// Cloud
+
+	
+	// Sky Light
+	// 스카이라이트 내 볼류메트릭 클라우드와 스카이 애트머스피어의 색상 캡쳐
+	SkyLight->SetRealTimeCapture(true);
+
+	// Fog
+	// 볼류메트릭 설정
+	Fog->SetVolumetricFog(true);
+	Fog->FogDensity = 0.03;
+
+	// Sky Atmosphere
+
+	// 초기화
+	Initialize();
+}
+
+
+// ----------------------------------------------------------------------
+// Overide
+void AEnvironmentManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// 
+	TimeUpdate(DeltaTime);
+	TimeCheck();
+	TransitionUpdate(DeltaTime);
+}
+
+void AEnvironmentManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 이니셜라이즈 함수 호출
+	Initialize();
+	
+}
+
+
+// ----------------------------------------------------------------------
+// Time
+void AEnvironmentManager::TimeUpdate(const float DeltaTime)
+{
+	// 세계 시간 규칙
+	RealElapsedTime += DeltaTime;
+	CurrentTime += DeltaTime / SecondsPerGameHour;
+
+	// 하루가 끝나면 처음으로 돌아가기
+	SetCurrentTime(CurrentTime);
+}
+
+void AEnvironmentManager::TimeCheck()
+{
+	if (RealElapsedTime > 0.5f)
+	{
+		if (not bIsTransitioning and bExistTransitionData)
+		{
+			float CheckingTime = TimeDivision * NextIndex;
+	
+			if (FMath::IsNearlyEqual(CheckingTime, CurrentTime, 0.1f))
+			{
+				StartTransition();
+			}
+		}
+	}
+}
+
+
+// ----------------------------------------------------------------------
+// Transition
+void AEnvironmentManager::StartTransition()
+{
+	// 진행 제어 변수
+	TransitionProgress = 0.0f;
+	bIsTransitioning = true;
+}
+
+void AEnvironmentManager::TransitionUpdate(const float DeltaTime)
+{
+	if (bIsTransitioning)
+	{
+		// 보간 알파 생성
+		TransitionProgress += DeltaTime / TransitionDuration;
+
+		FEnvironmentTimeData& CurrentData = EnvironmentTimeDataTable[CurrentIndex];
+		FEnvironmentTimeData& NextData = EnvironmentTimeDataTable[NextIndex];
+		
+		// Env 데이터
+		if (TransitionProgress >= 1.0f)
+		{
+			// 마지막 1.0f로 고정
+			// 설정 보간
+			LerpLightRotation(CurrentData,NextData,1);
+			LerpPostProcess(CurrentData,NextData,1);
+			LerpFog(CurrentData,NextData,1);
+			
+			// 트랜지션 진행 초기화
+			StopTransition();
+		}
+		
+		// 설정 보간
+		LerpLightRotation(CurrentData,NextData,TransitionProgress);
+		LerpPostProcess(CurrentData,NextData,TransitionProgress);
+		LerpFog(CurrentData,NextData,TransitionProgress);
+	}
+}
+
+void AEnvironmentManager::StopTransition()
+{
+	TransitionProgress = 1.0f;
+
+	// 인덱스 증가
+	SetTransitionIndex(PreviousIndex, CurrentIndex);
+	SetTransitionIndex(CurrentIndex, NextIndex);
+	SetTransitionIndex(NextIndex, CurrentIndex + 1);
+
+	// 인덱스 순회 지정
+	if (NextIndex > TotalIndex) NextIndex = 0;
+	
+	bIsTransitioning = false;
+}
+
+void AEnvironmentManager::NextTransition()
+{
+	float NextTime = EnvironmentTimeDataTable[NextIndex].Time;
+	SetCurrentTime(NextTime);
+	
+	StartTransition();
+}
+
+void AEnvironmentManager::PreviousTransition()
+{
+}
+
+
+// ----------------------------------------------------------------------
+// Lerp
+void AEnvironmentManager::LerpLightRotation(const FEnvironmentTimeData& Current, const FEnvironmentTimeData& Target, const float& Alpha)
+{
+	// Sun 설정 적용
+	if (Sun)
+	{
+		float CurrentY = CalculateSunRotation(Current.Time);
+		float TargetY = CalculateSunRotation(Target.Time);
+
+		float RotationCoefficient = Target.TimeRotationCoefficient;
+		FRotator CurrentRotation = FRotator::MakeFromEuler(FVector(0, CurrentY, 0));
+		FRotator TargetRotation = FRotator::MakeFromEuler(FVector(0, TargetY + RotationCoefficient, 0));
+
+		FRotator NewRotation = FRotator(FQuat::Slerp(CurrentRotation.Quaternion(),TargetRotation.Quaternion(),Alpha));
+		Sun->SetWorldRotation(NewRotation);
+	}
+}
+
+void AEnvironmentManager::LerpPostProcess(const FEnvironmentTimeData& Current, const FEnvironmentTimeData& Target, const float& Alpha)
+{
+	// PostProcess 설정 적용
+	if (PostProcess)
+	{
+		// 구조체 선형 보간
+		FEnvironmentPostProcess NewPostProcess = Current.PostProcess.LerpWith(Target.PostProcess, Alpha);
+
+		// 대입
+		PostProcess->Settings = NewPostProcess.ToPostProcessSettings();
+	}
+}
+
+void AEnvironmentManager::LerpFog(const FEnvironmentTimeData& Current, const FEnvironmentTimeData& Target, const float& Alpha)
+{
+	// Fog 설정 적용
+	 if (Fog)
+	 {
+	 	Fog->SetFogDensity(FMath::Lerp(Current.FogDensity, Target.FogDensity, Alpha));
+		Fog->SetFogHeightFalloff(FMath::Lerp(Current.FogHeightFalloff, Target.FogHeightFalloff, Alpha));
+	 }
+}
+
+
+// ---------------------------------------------------------------------
+// Initial
+void AEnvironmentManager::Initialize()
+{
+	// 환경 데이터 정렬 -> 트랜지션 인덱싱 -> 타임 데이터 셋
+	InitializePostProcess();
+	InitializeEnvironmentDataTable();
+	InitializeTransitionIndex();
+	InitalizeTimeData();
+}
+
+void AEnvironmentManager::InitializePostProcess()
+{
+	PostProcess->Settings.bOverride_WhiteTemp = true;
+	
+	PostProcess->Settings.bOverride_ColorSaturation = true;
+	PostProcess->Settings.bOverride_ColorContrast = true;
+	PostProcess->Settings.bOverride_ColorGamma = true;
+	PostProcess->Settings.bOverride_ColorGain = true;
+	PostProcess->Settings.bOverride_ColorOffset = true;
+
+	PostProcess->Settings.bOverride_AutoExposureMinBrightness = true;
+	PostProcess->Settings.bOverride_AutoExposureMaxBrightness = true;
+
+}
+
+void AEnvironmentManager::InitializeEnvironmentDataTable()
+{
+	EnvironmentTimeDataTable.Sort
+	(
+		[](const FEnvironmentTimeData& A, const FEnvironmentTimeData& B)
+		{
+			return A.Priority < B.Priority;  // 우선순위 오름차순 정렬
+		}
+	);
+
+	for (FEnvironmentTimeData& Data : EnvironmentTimeDataTable)
+	{
+		Data.PostProcess.bOverride_WhiteTemp = true;
+		Data.PostProcess.bOverride_ColorSaturation = true;
+		Data.PostProcess.bOverride_ColorContrast = true;
+		Data.PostProcess.bOverride_ColorGamma = true;
+		Data.PostProcess.bOverride_ColorGain = true;
+		Data.PostProcess.bOverride_ColorOffset = true;
+		Data.PostProcess.bOverride_AutoExposureMinBrightness = true;
+		Data.PostProcess.bOverride_AutoExposureMaxBrightness = true;
+	}
+}
+
+void AEnvironmentManager::InitializeTransitionIndex()
+{
+	const int32 SliceCount = EnvironmentTimeDataTable.Num();
+	if (SliceCount <= 0)
+	{
+		bExistTransitionData = false;
+		return;
+	}
+    
+	// 24시간을 슬라이스 수로 나누어 균등 배분
+	bExistTransitionData = true;
+	TotalIndex = SliceCount;
+	TimeDivision = 24.0f / SliceCount;
+
+	for (FEnvironmentTimeData& Data : EnvironmentTimeDataTable)
+	{
+		Data.Time = TimeDivision * Data.Priority;
+	}
+}
+
+void AEnvironmentManager::InitalizeTimeData()
+{
+	if (bExistTransitionData)
+	{
+		bIsTransitioning = false;
+		
+		// StartTime으로 설정
+		SetCurrentTime(StartTime);
+
+		int32 NewIndex = FindTimeDataIndexForTime(CurrentTime);
+
+		// StartTime에 해당하는 적절한 인덱스 찾기
+		SetTransitionIndex(PreviousIndex, CurrentIndex);
+		SetTransitionIndex(CurrentIndex, NewIndex);
+		SetTransitionIndex(NextIndex, CurrentIndex + 1);
+	
+		// 배열 데이터로 적용
+		const FEnvironmentTimeData& InitData = EnvironmentTimeDataTable[CurrentIndex];
+		UE_LOG(LogTemp, Warning, TEXT("New : %d"), NewIndex);
+	
+		// 해 로테이션 초기화
+		float NewPitch = CalculateSunRotation(InitData.Time);
+		float RotationCoefficient = InitData.TimeRotationCoefficient;
+		FRotator NewRotation = FRotator::MakeFromEuler(FVector(0, NewPitch + RotationCoefficient, 0));
+		Sun->SetWorldRotation(NewRotation);
+	
+		// 포스트 프로세스 구조체 초기화
+		PostProcess->Settings = InitData.PostProcess.ToPostProcessSettings();
+
+		// 포그 초기화
+		Fog->SetFogDensity(InitData.FogDensity);
+		Fog->SetFogHeightFalloff(InitData.FogHeightFalloff);
+	}
+}
+
+
+// ----------------------------------------------------------------------
+// Calculate
+float AEnvironmentManager::CalculateSunRotation(const float Time)
+{
+	// 24시간 기준
+	float Result = (360.0f * (Time * (1/24.0f)) + 90.0f);
+	return Result;
+}
+
+int32 AEnvironmentManager::FindTimeDataIndexForTime(const float Time)
+{
+	if (!bExistTransitionData || EnvironmentTimeDataTable.Num() == 0) return -1;
+    
+	// 균등 분할된 시간 기준으로 인덱스 계산
+	int32 Index = FMath::FloorToInt(Time / TimeDivision);
+	
+	// 인덱스 범위 제한
+	return FMath::Clamp(Index, 0, TotalIndex);
+}
+
+void AEnvironmentManager::SetTransitionIndex(int32& Index, int32 NewIndex)
+{
+	Index = NewIndex;
+	
+	if (Index >= TotalIndex) Index = 0;
+	if (Index < 0) Index = TotalIndex;
+}
+
+void AEnvironmentManager::SetCurrentTime(float NewTime)
+{
+	// 24시간 시스템에서 순환 처리
+	CurrentTime = FMath::Fmod(NewTime, 24.0f);
+    
+	// 음수 시간 처리 (만약 NewTime이 음수일 경우)
+	if (CurrentTime < 0.0f)
+	{
+		CurrentTime += 24.0f;
+	}
+}
+
+void AEnvironmentManager::DebugTransitionIndex()
+{
+	if (bExistTransitionData)
+	{
+		bIsTransitioning = false;
+
+		// StartTime에 해당하는 적절한 인덱스 찾기
+		SetTransitionIndex(PreviousIndex, DebuggingIndex - 1);
+		SetTransitionIndex(CurrentIndex, DebuggingIndex);
+		SetTransitionIndex(NextIndex, DebuggingIndex + 1);
+
+
+		// 배열 데이터로 적용
+		const FEnvironmentTimeData& InitData = EnvironmentTimeDataTable[CurrentIndex];
+
+		// StartTime으로 설정
+		SetCurrentTime(InitData.Time);
+	
+		// 해 로테이션 초기화
+		float NewPitch = CalculateSunRotation(InitData.Time);
+		float NewRotationCoefficient = InitData.TimeRotationCoefficient;
+		FRotator NewRotation = FRotator::MakeFromEuler(FVector(0, NewPitch + NewRotationCoefficient, 0));
+		Sun->SetWorldRotation(NewRotation);
+	
+		// 포스트 프로세스 구조체 초기화
+		PostProcess->Settings = InitData.PostProcess.ToPostProcessSettings();
+
+		// 포그 초기화
+		Fog->SetFogDensity(InitData.FogDensity);
+		Fog->SetFogHeightFalloff(InitData.FogHeightFalloff);
+	}
+}
+
---
+++ Source/Project_GGF/Public/World/EnvironmentManager.h
@@ -0,0 +1,236 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/FEnvironmentTimeData.h"
+#include "GameFramework/Actor.h"
+#include "Components/VolumetricCloudComponent.h"
+#include "Components/PostProcessComponent.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Components/ExponentialHeightFogComponent.h"
+#include "Components/SkyAtmosphereComponent.h"
+#include "Components/SkyLightComponent.h"
+#include "EnvironmentManager.generated.h"
+
+UCLASS()
+class PROJECT_GGF_API AEnvironmentManager : public AActor
+{
+	GENERATED_BODY()
+	
+	//——————————————————————————————————————————————————————————————————————
+	// 생성자 & 소멸자
+	//——————————————————————————————————————————————————————————————————————
+	AEnvironmentManager();
+
+private:
+	//——————————————————————————————————————————————————————————————————————
+	// 프로퍼티 & 변수
+	//——————————————————————————————————————————————————————————————————————
+	// Non-reflected
+
+	// Rule
+	// UPROPERTY(VisibleAnywhere)
+	// float CurrentTime = 0.0f;
+
+	// Time
+	UPROPERTY(VisibleAnywhere, Category = "Time")
+	float CurrentTime = 0.0f;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Time")
+	float RealElapsedTime = 0.0f;
+
+	UPROPERTY(VisibleAnywhere, Category = "Time")
+	float TimeDivision = 0.0f;
+
+	UPROPERTY(VisibleAnywhere, Category = "Time")
+	bool bIsInitialStart = true;
+	
+	// Transition Index
+	UPROPERTY(VisibleAnywhere, Category = "Index")
+	bool bExistTransitionData = false;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Index")
+	int32 TotalIndex = -1;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Index")
+	int32 PreviousIndex = -1;
+
+	UPROPERTY(VisibleAnywhere, Category = "Index")
+	int32 CurrentIndex = 0;
+
+	UPROPERTY(VisibleAnywhere, Category = "Index")
+	int32 NextIndex = 1;
+
+	
+	// Transition
+	UPROPERTY(VisibleAnywhere, Category = "Transition")
+	bool bIsTransitioning = false;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Transition")
+	float TransitionProgress = 1.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Transition")
+	float TransitionDuration = 3.0f;
+	
+	// Timer
+	UPROPERTY(VisibleAnywhere)
+	FTimerHandle TimeUpdateTimer;
+
+	// Sun Rotation
+
+
+	//——————————————————————————————————————————————————————————————————————
+	// 함수
+	//——————————————————————————————————————————————————————————————————————
+	// 오버라이드
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+	
+	// Time
+	UFUNCTION()
+	void TimeUpdate(float DeltaTime);
+
+	void TimeCheck();
+
+	
+	// Transition
+	// 트랜지션은 시작한 후, 틱 마다 특정 시간 동안 0->1값으로 점진적으로 다음 인덱스로 보간된다.
+	UFUNCTION()
+	void StartTransition();
+	
+	UFUNCTION()
+	void TransitionUpdate(float DeltaTime);
+	
+	UFUNCTION()
+	void StopTransition();
+	
+	
+	// Debugging
+	UFUNCTION(CallInEditor, Category="EnvironmentManager")
+	void NextTransition();
+
+	UFUNCTION(CallInEditor, Category="EnvironmentManager")
+	void PreviousTransition();
+
+
+	// Lerp
+	UFUNCTION()
+	void LerpLightRotation(const FEnvironmentTimeData& Current, const FEnvironmentTimeData& Target, const float& Alpha);
+
+	UFUNCTION()
+	void LerpPostProcess(const FEnvironmentTimeData& Current, const FEnvironmentTimeData& Target, const float& Alpha);
+
+	UFUNCTION()
+	void LerpFog(const FEnvironmentTimeData& Current, const FEnvironmentTimeData& Target, const float& Alpha);
+
+	
+	// Initial
+	UFUNCTION(CallInEditor, Category="EnvironmentManager")
+	void Initialize();
+
+	UFUNCTION()
+	void InitializePostProcess();
+
+	UFUNCTION()
+	void InitializeTransitionIndex();
+
+	UFUNCTION()
+	void InitalizeTimeData();
+
+	UFUNCTION()
+	void InitializeEnvironmentDataTable();
+
+
+	// Calculate
+	UFUNCTION()
+	float CalculateSunRotation(const float Time);
+	
+	UFUNCTION()
+	int32 FindTimeDataIndexForTime(const float Time);
+	
+	// Setter
+	UFUNCTION()
+	void SetTransitionIndex(int32& Index, int32 NewIndex);
+
+	UFUNCTION()
+	void SetCurrentTime(float NewTime);
+
+protected:
+	//——————————————————————————————————————————————————————————————————————
+	// 프로퍼티 & 변수
+	//——————————————————————————————————————————————————————————————————————
+	// Non-reflected
+
+
+	// Rule
+	/**
+	 * @brief 게임의 한 시간을 실제 초 단위로 나타낸 규칙 값
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Time")
+	float SecondsPerGameHour  = 60.0f;
+
+	// Time
+	// TODO: 시작 시간 -> 끝 시간을 제한시켜 순회하는 맵의 키를 무시하는 형태로?
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Time", meta=(UIMin = "0.0", UIMax = "24.0"))
+	float StartTime = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Time", meta=(UIMin = "0.0", UIMax = "24.0"))
+	float EndTime = 24.0f;
+	
+	// Component
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Light | Sun")
+	UDirectionalLightComponent* Sun;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Light | Moon")
+	UDirectionalLightComponent* Moon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Environment")
+	UVolumetricCloudComponent* Cloud;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Environment")
+	USkyLightComponent* SkyLight;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Environment")
+	USkyAtmosphereComponent* SkyAtmosphere;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Environment")
+	UExponentialHeightFogComponent* Fog;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Environment")
+	UStaticMeshComponent* SkyMesh;
+
+
+	// PostProcessVolume
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Environment")
+	UPostProcessComponent* PostProcess;
+
+	// EnvironmentTimeData
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnvironmentTimeData")
+	TArray<FEnvironmentTimeData> EnvironmentTimeDataTable;
+
+public:
+	//——————————————————————————————————————————————————————————————————————
+	// 함수
+	//——————————————————————————————————————————————————————————————————————
+	// 오버라이드
+
+	// Debugging
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debugging")
+	int32 DebuggingIndex = 0;
+
+	UFUNCTION(CallInEditor, Category="Debugging")
+	void DebugTransitionIndex();
+
+
+	//——————————————————————————————————————————————————————————————————————
+	// Get & Set
+	//——————————————————————————————————————————————————————————————————————
+	// Getter
+
+	
+	// Setter
+
+	
+};
